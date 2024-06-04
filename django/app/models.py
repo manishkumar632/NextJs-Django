43bc4690ce833e23f958803a1c71e3d5011b0d1b
manishkumar632 <manishmu632@gmail.com>
2024-06-04 16:10:39 +0530

    pip install cloudinary

--- django/app/models.py
+++ django/app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class App(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    slug = models.SlugField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
